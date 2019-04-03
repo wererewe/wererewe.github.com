3c5963faf9ed60a8f7e5ccdba01429d8f438c6ce
kyungjin <kyungjin@29cm.co.kr>
2019-04-03 14:31:23 +0900

    modify initial post

---
+++ _posts/2019-04-02-initial-post.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "첫 포스팅"
+date:   2019-04-02 12:24:00 +0900
+categories: dev
+---
+처음 써본당!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser":null},{
+        "title": "첫 포스팅",
+        "excerpt":"처음 써본당!  ","categories": ["dev"],
+        "tags": [],
+        "url": "http://localhost:4000/dev/initial-post/",
         "teaser":null}]
